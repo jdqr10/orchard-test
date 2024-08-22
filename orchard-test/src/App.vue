2fe0620b0b551298a1a79cf6f03446799ef3d93e
Jerónimo Duque <jeronimo.duquer@uqvirtual.edu.co>
2024-08-21 20:32:57 -0500

    first component

--- orchard-test/src/App.vue
+++ orchard-test/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="wrap"></div>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld1 msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld1 from './components/HelloWorld1.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloWorld1
   }
 }
 </script>
@@ -20,7 +23,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #d3e0ed;
+  
+}
+.wrap{
+  background-color: #181717;
+
 }
 </style>
